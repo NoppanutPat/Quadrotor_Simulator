22a9230e108d089c34c51ab1d28c0691baaca3b3
Noppanut <noppanut.t@ku.th>
2020-08-21 12:18:06 +0700

    First Commit

---
+++ build/quad_sim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pat/quad_sim/src/quad_sim
+# Build directory: /home/pat/quad_sim/build/quad_sim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
